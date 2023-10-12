6b1339900c39db3bfa9162d1aeafb73ef8040767
Josh Jacobson <josh.jacobson@Joshs-MacBook-Pro.local>
2023-10-12 16:17:07 -0400

    Update households per sq mi to use thousands

--- client/src/constants/householdsPerSqMile.js
+++ client/src/constants/householdsPerSqMile.js
@@ -1,15 +1,15 @@
 export const householdsPerSqMile = {
-  'R - Very Low': 12,
-  'R - Low': 15, // Residential-Low Density
-  'R - Moderate': 23, // Residential-Moderate Density
-  'R - Medium': 31, // Residential-Medium Density
-  'R - High': 45,
-  'MU - Moderate': 31, // Commercial-Moderate Density
-  'MU - Medium': 38, // Commercial-Medium Density
-  'MU - High': 77, // Commercial-High Density
-  'PDR - Medium': 45, // Institutional
-  'PDR - High': 77, // Federal
-  'D - High': 115, // Local Public Facilities
+  'R - Very Low': 12000,
+  'R - Low': 15000, // Residential-Low Density
+  'R - Moderate': 23000, // Residential-Moderate Density
+  'R - Medium': 31000, // Residential-Medium Density
+  'R - High': 45000,
+  'MU - Moderate': 31000, // Commercial-Moderate Density
+  'MU - Medium': 38000, // Commercial-Medium Density
+  'MU - High': 77000, // Commercial-High Density
+  'PDR - Medium': 45000, // Institutional
+  'PDR - High': 77000, // Federal
+  'D - High': 115000, // Local Public Facilities
   'Park': 0, // Parks, Recreation, and Open Space
-  'D - Fuck the Height Act': 192 // Production, Distribution and Repair
+  'D - Fuck the Height Act': 192000 // Production, Distribution and Repair
 };

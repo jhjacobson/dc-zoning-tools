e98444f93074bb523de46bdbb8092ee561dd86d2
Josh Jacobson <josh.jacobson@Joshs-MacBook-Pro.local>
2023-10-12 15:44:03 -0400

    reorganize zoning list

--- client/src/constants/zoningColors.js
+++ client/src/constants/zoningColors.js
@@ -1,5 +1,6 @@
 // src/constants/zoningColors.js
 export const zoningColors = {
+  'Park': '#7B68EE', // Parks, Recreation, and Open Space
   'R - Very Low': '#FFA07A',
   'R - Low': '#FFA07A', // Residential-Low Density
   'R - Moderate': '#20B2AA', // Residential-Moderate Density
@@ -11,6 +12,5 @@ export const zoningColors = {
   'PDR - Medium': '#00FF7F', // Institutional
   'PDR - High': '#00FF7F', // Federal
   'D - High': '#00FF7F', // Local Public Facilities
-  'Park': '#7B68EE', // Parks, Recreation, and Open Space
   'D - Fuck the Height Act': '#7B68EE' // Production, Distribution and Repair
 };
